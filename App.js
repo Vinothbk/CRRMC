4c192d3e07a6b2c960449d0e26a07b8a3a1f5a55
samjebadurai <sam@rinsoft-pvtltd.com>
2020-10-08 17:58:35 +0530

    Login Screen

--- App.js
+++ App.js
@@ -1,105 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+/* This is an Login Registration example from https://aboutreact.com/ */
+/* https://aboutreact.com/react-native-login-and-signup/ */
 
+//Import React
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  ImageBackground
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//Import Navigators from React Navigation
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <ImageBackground
-              accessibilityRole={'image'}
-              source={require('./app/Images/CRRLogo.jpg')}
-              style={styles.background}
-              imageStyle={styles.logo}>
-            </ImageBackground>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+//Import all the screens needed
+import SplashScreen from './app/Screen/SplashScreen';
+import LoginScreen from './app/Screen/LoginScreen';
+import RegisterScreen from './app/Screen/RegisterScreen';
+import DrawerNavigationRoutes from './app/Screen/DrawerNavigationRoutes';
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+const Auth = createStackNavigator({
+  //Stack Navigator for Login and Sign up Screen
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: {
+      headerShown: false,
+    },
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  background: {
-    paddingBottom: 140,
-    paddingTop: 80,
-    paddingHorizontal: 32,
-    backgroundColor: Colors.lighter,
-  },
-  logo: {
-    overflow: 'visible',
-    resizeMode: 'cover',
-  },
-  text: {
-    fontSize: 40,
-    fontWeight: '600',
-    textAlign: 'center',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  RegisterScreen: {
+    screen: RegisterScreen,
+    navigationOptions: {
+      title: 'Register',
+      headerStyle: {
+        backgroundColor: '#fff',
+      },
+      headerTintColor: '#307ecc',
+    },
   },
 });
 
-export default App;
+/* Switch Navigator for those screens which needs to be switched only once
+  and we don't want to switch back once we switch from them to the next one */
+const App = createSwitchNavigator({ 
+  SplashScreen: {
+    /* SplashScreen which will come once for 5 Seconds */
+    screen: SplashScreen,
+    navigationOptions: {
+      /* Hiding header for Splash Screen */
+      headerShown: false,
+    },
+  },
+  Auth: {
+    /* Auth Navigator which includer Login Signup will come once */
+    screen: Auth,
+  },
+  DrawerNavigationRoutes: {
+    /* Navigation Drawer as a landing page */
+    screen: DrawerNavigationRoutes,
+    navigationOptions: {
+      /* Hiding header for Navigation Drawer as we will use our custom header */
+      headerShown: false,
+    },
+  },
+});
+
+export default createAppContainer(App);
